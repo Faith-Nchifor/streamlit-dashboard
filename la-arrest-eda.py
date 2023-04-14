5c74f031b7351e4aa45bb49ebdaf3dede46aca9c
Faith Wenyi Nchifor <52848633+Faith-Nchifor@users.noreply.github.com>
2023-04-14 23:43:25 +0100

    Update la-arrest-eda.py

--- la-arrest-eda.py
+++ la-arrest-eda.py
@@ -1,40 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # EDA for LA crime datasets
-# The datasets involved are:
-# * Arrest data from 2010-2020
-# * Crime data from 2010-2020
-# ###### More ideas are welcome
-
-# In[1]:
-
-
+import streamlit as st
 
 
 import numpy as np # linear algebra
 import pandas as pd 
-pd.option_context('display.max_columns', None, 'display.max_rows', None)
 import matplotlib.pyplot as plt
 import seaborn as sns
+#import folium
+#pd.option_context('display.max_columns', None, 'display.max_rows', None)
+
+
 
 # In[2]:
 
+#arrest_data=pd.read_csv('arrest_cleaned.csv',parse_dates=['Arrest Date','Booking Date'])
+#crime_types=pd.read_csv('crime-cleaned.csv',parse_dates=['Date Rptd','DATE OCC'])
+df=pd.read_csv('sports-political-donations.csv')
+print(df.head())
+print(df['Team'].value_counts().reset_index())
+parties=df['Party'].value_counts().reset_index()
 
-arrest_data=pd.read_csv('arrest_cleaned.csv',parse_dates=['Arrest Date','Booking Date'])
-crime_types=pd.read_csv('crime-cleaned.csv',parse_dates=['Date Rptd','DATE OCC'])
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.bar_chart(parties, x='index',y='Party')
+
+
+m = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+columns=['lat', 'lon'])
+
+st.map(m)
+
+
+fig, ax = plt.subplots()
+#plt.plot(df['Team'].value_counts())
+sns.countplot(y='Team',data=df)
+st.pyplot(fig)
+
+st.line_chart(df['Team'].value_counts().reset_index(), x='Team')
 
 
 
-
-
-crime_years_df=crime_types.groupby('Date Rptd').size()
-#sns.baplot(data=crime_years_df, x='Date Rptd',y='Vict Age')
-plt.hist([2,3,5,5,3,1,2])
-plt.show()
-
-
-# In[ ]:
 
 
 
